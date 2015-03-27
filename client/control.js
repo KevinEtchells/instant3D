6190ef70696260eb7850e7db0b082e1ed0d1c712
Kevin Etchells <kevin@KevLaptop>
2015-03-27 07:41:49 +0000

    Image uploads now limit/reduce resolution therefore file-size

--- client/control.js
+++ client/control.js
@@ -90,59 +90,48 @@ Template.control.events({
 	'change input.upload': function(event) {
 			var docId = Docs.findOne()._id;
 
-
-/*
-var filesToUpload = input.files;
-var file = filesToUpload[0];
-
-var img = document.createElement("img");
-var reader = new FileReader();  
-reader.onload = function(e) {img.src = e.target.result}
-reader.readAsDataURL(file);
-
-var ctx = canvas.getContext("2d");
-ctx.drawImage(img, 0, 0);
-
-var MAX_WIDTH = 800;
-var MAX_HEIGHT = 600;
-var width = img.width;
-var height = img.height;
-
-if (width > height) {
-  if (width > MAX_WIDTH) {
-    height *= MAX_WIDTH / width;
-    width = MAX_WIDTH;
-  }
-} else {
-  if (height > MAX_HEIGHT) {
-    width *= MAX_HEIGHT / height;
-    height = MAX_HEIGHT;
-  }
-}
-canvas.width = width;
-canvas.height = height;
-var ctx = canvas.getContext("2d");
-ctx.drawImage(img, 0, 0, width, height);
-
-var dataurl = canvas.toDataURL("image/png");
-*/
-
 		_.each(event.target.files, function(file) {
 
 			var fileReader = new FileReader();
 			fileReader.onload = function(file) {
 
-				var name = event.target.value.replace('C:\\fakepath\\', '');	
+				var name = event.target.value.replace('C:\\fakepath\\', '').replace(/\./g, '') + '.png';
 				
 				var img = new Image();
 
 				img.onload = function() {
+					
+					var MAX_WIDTH = 800;
+					var MAX_HEIGHT = 600;
+					var width = this.width;
+					var height = this.height;
+
+					if (width > height) {
+						if (width > MAX_WIDTH) {
+							height *= MAX_WIDTH / width;
+							width = MAX_WIDTH;
+						}
+					} else {
+						if (height > MAX_HEIGHT) {
+							width *= MAX_HEIGHT / height;
+							height = MAX_HEIGHT;
+						}
+					}
+					
+					var canvas = document.createElement('canvas');
+					var ctx = canvas.getContext('2d');
+					canvas.width = width;
+					canvas.height = height;
+					ctx.drawImage(img, 0, 0, width, height);
+
 					Resources.insert({
 						name: name,
-						file: file.target.result,
+						//file: file.target.result,
+						file: canvas.toDataURL('image/png'),
 						doc: docId,
 						aspectRatio: this.width / this.height
 					});
+
 				};
 
 				img.src = file.target.result;
